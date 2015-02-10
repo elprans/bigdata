c258080e7ec2d891787423f47a8befc1ae44213a
Mike Bayer <mike_mp@zzzcomputing.com>
2015-02-10 14:32:07 -0500

    dev

--- bigdata/dictionary_from_raw.py
+++ bigdata/dictionary_from_raw.py
@@ -2,6 +2,143 @@ import re
 import os
 
 
+class Matrix(object):
+    def __init__(self, rec):
+        self.code = rec[1][0]
+        self.name = rec[1][1]
+        self.expected_items = int(rec[1][2])
+        self.universe = None
+        self.aggregates = []
+        self.entries = []
+        self.current_aggregate = None
+        self.scale = None
+
+    def confirm(self):
+        count = 0
+
+        stack = list(self.aggregates)
+        while stack:
+            agg = stack.pop(0)
+            count += 1
+            count += len(agg.entries)
+            stack.extend(agg.children)
+
+        count += len(self.entries)
+
+        if count != self.expected_items:
+            print "BAD COUNT: %s  %d != %d" % (self.code, count, self.expected_items)
+            #if self.code == "PCT17D":
+            #    import pdb
+            #    pdb.set_trace()
+        #assert count == self.expected_items
+
+    def _push_aggregate(self, rec):
+        new_agg = Aggregate(self, rec[1][0], rec[1][1] == 'N|')
+        if not self.current_aggregate:
+            self.aggregates.append(new_agg)
+        elif not new_agg.nest and self.current_aggregate.entries:
+            # sibling case
+
+            parent = self.current_aggregate.parent
+            #while parent is not None and parent.entries:
+            #    parent = parent.parent
+
+            new_agg.parent = parent
+            if new_agg.parent is None:
+                self.aggregates.append(new_agg)
+            else:
+                parent.children.append(new_agg)
+        else:
+            new_agg.parent = self.current_aggregate
+            self.current_aggregate.children.append(new_agg)
+        self.current_aggregate = new_agg
+
+    def _push_repeat(self, rec):
+        if self.current_aggregate.parent:
+            to_repeat = self.current_aggregate.parent.children[-2]
+            to_repeat.copy_into(self.current_aggregate)
+        else:
+            assert False
+
+    def _push_entry(self, entry):
+        if not self.current_aggregate:
+            self.entries.append(entry[1][0])
+        else:
+            self.current_aggregate.entries.append(entry[1][0])
+
+    def receive_rec(self, rec):
+        if rec[0] == 'universe':
+            self.universe = rec[1][0]
+        elif rec[0] == 'scale':
+            self.scale = rec[1][0]
+        elif rec[0] == 'aggregate':
+            self._push_aggregate(rec)
+        elif rec[0] == 'plain':
+            self._push_entry(rec)
+        elif rec[0] == 'repeat':
+            self._push_repeat(rec)
+        else:
+            assert False
+
+    def __str__(self):
+        text = "\n%s: %s [%s]" % (self.code, self.name, self.expected_items)
+        for entry in self.entries:
+            text += "\n %s" % entry
+        for agg in self.aggregates:
+            text += agg._as_string(1)
+        return text
+
+
+class Aggregate(object):
+    def __init__(self, matrix, name, nest=False):
+        self.matrix = matrix
+        self.name = name
+        self.parent = None
+        self.nest = nest
+        self.children = []
+        self.entries = []
+
+    def _as_string(self, indent):
+        text = "\n\n%s%s" % ("  " * indent, self.name)
+        for entry in self.entries:
+            text += "\n%s%s" % ("  " * (indent + 1), entry)
+        for agg in self.children:
+            text += agg._as_string(indent + 1)
+        return text
+
+    def _has_aggregate_entries(self):
+        if self.entries:
+            return True
+        else:
+            for ent in self.children:
+                if ent._has_aggregate_entries():
+                    return True
+        return False
+
+    def copy_into(self, agg):
+        agg.entries.extend(self.entries)
+        for child in self.children:
+            new_child = Aggregate(self.matrix, child.name)
+            new_child.parent = agg
+            agg.children.append(new_child)
+            child.copy_into(new_child)
+
+
+def _create_matrices(iter_):
+    current_matrix = None
+    for entry in iter_:
+        #print entry
+        if entry[0] == 'matrix_desc_full':
+            if current_matrix is not None:
+                current_matrix.confirm()
+                yield current_matrix
+            current_matrix = Matrix(entry)
+        else:
+            current_matrix.receive_rec(entry)
+    if current_matrix:
+        yield current_matrix
+
+
 def _ignores(iter_):
     ignore = [
         re.compile(r"^Chapter"),
@@ -57,7 +194,7 @@ def _derive_tokens(iter_):
         ),
 
         (
-            "aggregate", re.compile("^[^\:]+\:[^\:]*$"), (0, )
+            "aggregate", re.compile("^(N\|)?([^\:]+\:[^\:]*)$"), (2, 1)
         ),
 
         (
@@ -67,7 +204,6 @@ def _derive_tokens(iter_):
         (
             "ends_in_trailer", re.compile(r".*[;\-,]$"), (0,)
         ),
-
         (
             "continuation", re.compile(r"^[a-z].*$"), (0, )
         ),
@@ -77,6 +213,10 @@ def _derive_tokens(iter_):
 
         (
             "ellipses", re.compile(r"^\.$"), (0, )
+        ),
+
+        (
+            "scale", re.compile(r".*—$"), (0,)
         ),
 
         (
@@ -227,8 +367,9 @@ def _parse(fh):
     iter_ = _merge_ends_in_trailer(iter_)
     iter_ = _fill_ellipses(iter_)
     iter_ = _combine_tokens(iter_)
-    for line in iter_:
-        yield line
+    iter_ = _create_matrices(iter_)
+    for rec in iter_:
+        yield rec
 
 
 def run():
